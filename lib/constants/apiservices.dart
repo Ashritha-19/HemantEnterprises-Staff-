9772718eb5ffbbb2631b8fa22700c356661bf2e2
Ashritha-19 <ashri.vig2337@gmail.com>
2025-03-21 17:05:14 +0530

    fourth commit

--- lib/constants/apiservices.dart
+++ lib/constants/apiservices.dart
@@ -4,4 +4,6 @@ static const String baseUrl = "https://hemanthapp.whysocial.in/public";
 
 static const String login = "/staffauth/login";
 
+
+
 }
--- lib/views/brands/home.dart
+++ lib/views/brands/home.dart
@@ -1,11 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_sizer/flutter_sizer.dart';
+// ignore_for_file: unnecessary_brace_in_string_interps, avoid_print
+
+import 'dart:convert';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:hemantenterprisesstaff/constants/apiservices.dart';
 import 'package:hemantenterprisesstaff/constants/colorconstants.dart';
 import 'package:hemantenterprisesstaff/constants/imageconstants.dart';
 import 'package:hemantenterprisesstaff/constants/searchfield.dart';
 import 'package:hemantenterprisesstaff/routes/app_routes.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,28 +22,51 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<String> brands = [
-    Imageconstants.hindware,
-    Imageconstants.jaguar,
-    Imageconstants.brizo,
-    Imageconstants.queo,
-    Imageconstants.franke,
-    Imageconstants.grohe,
-  ];
+  List<dynamic> brandsList = [];
 
-  final List<String> brandNames = [
-    "Hindware",
-    "Jaguar",
-    "Brizo",
-    "Queo",
-    "Franke",
-    "Grohe",
-  ];
+  @override
+  void initState() {
+    super.initState();
+    fetchBrandData();
+  }
+  
+  Future<void> fetchBrandData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('access_token');
+    final url = 'https://hemanthapp.whysocial.in/public/brands/getbrandlist';
+
+    Map<String, String> headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer $token"
+    };
+
+    try {
+      final response = await http.get(Uri.parse(url), headers: headers);
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+        print(responseData.toString());
+
+        setState(() {
+          brandsList = responseData['banners'] ?? [];
+          
+        });
+        print('${brandsList.toString()}>>>>>>>>>>>>><<<<<<<<<<<<<');
+      } else {
+        Fluttertoast.showToast(msg: "Data not found");
+      }
+    } catch (e) {
+      Fluttertoast.showToast(msg: "Error: ${e.toString()}");
+    }
+  }
+
+  final String brandImgUrl = ApiConstants.brandImgBaseUrl;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        fit: StackFit.expand, // Ensures Stack covers the full screen
         children: [
           // Background image
           Positioned.fill(
@@ -44,88 +74,104 @@ class _HomeScreenState extends State<HomeScreen> {
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage(Imageconstants.background),
-                  fit: BoxFit.cover,
+                  fit:
+                      BoxFit.cover, // Ensures the image fully covers the screen
                   opacity: 0.2,
                 ),
               ),
             ),
           ),
           // Main content
-          Column(
-            children: [
-              // Reusable Custom AppBar
-              CustomAppBar(
-                hintText: "Search",                
-                onSearchTap: () {
-                  // Define action when search bar is tapped
-                },
-              ),
-               SizedBox(height: 5.h),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Text(
-                      "All Brands",
-                      style: GoogleFonts.instrumentSans(
-                        fontSize: 14,
-                        color: Colorconstants.darkBlack,
-                        fontWeight: FontWeight.w500,
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                // Reusable Custom AppBar
+                CustomAppBar(
+                  hintText: "Search",
+                  onSearchTap: () {
+                    // Define action when search bar is tapped
+                  },
+                ),
+                SizedBox(height: 5.h),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        "All Brands",
+                        style: GoogleFonts.instrumentSans(
+                          fontSize: 14,
+                          color: Colorconstants.darkBlack,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
-                  ),
-                   SizedBox(height: 2.h),
-                  // Brands grid
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: GridView.builder(
-                      padding: const EdgeInsets.only(top: 10),
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 6,
-                        crossAxisSpacing: 20,
-                        mainAxisSpacing: 20,
-                      ),
-                      itemCount: brands.length * 3,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        final brand = brands[index % brands.length];
-                        final brandName = brandNames[index % brandNames.length];
-                        return GestureDetector(
-                          onTap: () {
-                            Get.toNamed(
-                              AppRoutes.brandDetails,
-                              arguments: {
-                                'image': brand,
-                                'name': brandName,
+                    SizedBox(height: 2.h),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: brandsList.isEmpty
+                          ? Center(
+                              child:
+                                  CircularProgressIndicator()) // Show loading indicator
+                          : LayoutBuilder(
+                              builder: (context, constraints) {
+                                return GridView.builder(
+                                  shrinkWrap: true,
+                                  physics: NeverScrollableScrollPhysics(),
+                                  gridDelegate:
+                                      SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 3,
+                                    crossAxisSpacing: 8,
+                                    mainAxisSpacing: 8,
+                                  ),
+                                  itemCount: brandsList.length,
+                                  itemBuilder: (context, index) {
+                                    String brandImg = brandsList[index]['brand_img'] ?? '';
+                                    String brandName = brandsList[index]['brand_name'] ?? '';
+                                    String brandDescription = brandsList[index]['brand_description'] ?? '';
+                                    return GestureDetector(
+                                      onTap: () {
+                                        print('Brand tapped: ${brandsList[index]['brand_name']}');
+                                        Get.toNamed(AppRoutes.category, arguments: {
+                                          'brandImg' : brandImg,
+                                          'brandName' : brandName,
+                                          'brandDescription' : brandDescription,
+                                        });
+                                      },
+                                      child: Container(
+                                        height: 100,
+                                        decoration: BoxDecoration(
+                                          color: Colorconstants.white,
+                                          borderRadius:
+                                              BorderRadius.circular(100),
+                                        ),
+                                        child: Transform.scale(
+                                          scale: 0.5,
+                                          child: Image.network(
+                                            '${brandImgUrl}${brandImg}',
+                                            height: 5,
+                                            width: 5,
+                                            errorBuilder:
+                                                (context, error, stackTrace) =>
+                                                    Icon(Icons.broken_image,
+                                                        size: 50),
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                );
                               },
-                            );
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(50),
-                              border: Border.all(
-                                color: Colorconstants.brandLogoCircle,
-                                width: 1.0
-                              )
                             ),
-                            padding: const EdgeInsets.all(8.0),
-                            child: Image.asset(
-                              brand,
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                        );
-                      },
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
-      ),  
+      ),
     );
   }
 }
